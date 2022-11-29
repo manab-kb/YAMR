ae96319592d113ab91fd3bdc7e33e3df658b7a69
Manab Biswas <77844663+manab-kb@users.noreply.github.com>
2022-11-29 22:49:57 +0000

    Add files via upload

--- Connections.py
+++ Connections.py
@@ -24,5 +24,5 @@ class Connections():
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((HOST, PORT))
-            s.sendall(b"Hello, world")
+            s.sendall(b"Connected")
             data = s.recv(1024)
--- Nodes.py
+++ Nodes.py
@@ -4,30 +4,31 @@ from multiprocessing import *
 from Connections import *
 import time
 from filesplit.split import Split
+import shutil
+
 
 class WorkerNode(Connections):
 
     def __init__(self, port):
         self.port = port
 
+        #sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #result = sock.connect_ex(('127.0.0.1',port))
+        #if result == 0:
+
         pworker = Process(target = self.Server, args =(self.port,))
         pworker.start()
-        
+
         pathname = os.getcwd() + '/Storage' + str(self.port-2000)
-        
+        self.pathname = pathname
+
         if not os.path.isdir(pathname):
             os.mkdir(pathname)
     
-    def Write(self, filename, num_workers):
-        file = os.getcwd() + '/' + filename
-        splitsize = int(os.path.getsize(file) / num_workers)
-        outputpath = os.getcwd() + '/Storage1'
-
-        split = Split(file, outputpath)
-        split.bysize(splitsize)
-
-    def Read(self, filename):
-        pass
+    def Write(self, filepath, filename):
+        newpath = self.pathname + '/' + filename
+        filepath = filepath + filename
+        os.rename(filepath, newpath)
 
 
 class MasterNode(Connections):
@@ -38,29 +39,46 @@ class MasterNode(Connections):
         for i in range(1, self.num_workers+1):
             port = 2000 + i
             self.WorkerNode = WorkerNode(port)
-        
+
         pmain = Process(target = self.Server, args =(2000,))
         pmain.start()
 
         for i in range(1,self.num_workers+1):
             self.Client(2000+i)
-    
+
     def stats(self):
         print('PPID:', os.getppid())
         print('PID:', os.getpid())
-    
-    def WriteFile(self, filename):
-        self.WorkerNode.Write(filename, self.num_workers)
-
-    def ReadFile(self, filename):
-        pass
-
-    def MapReduce(self):
-        pass
 
     def Q(self):
         pass
 
+
 a = MasterNode(3)
-time.sleep(1000)
-a.WriteFile('input.txt')
+
+
+def Write(filename, num_workers):
+    file = os.getcwd() + '/' + filename
+    splitsize = int(os.path.getsize(file) / num_workers)
+
+    outputpath = os.getcwd() + '/TempOutputs/'
+    if not os.path.isdir(outputpath):
+        os.mkdir(outputpath)
+    
+    split = Split(file, outputpath)
+    split.bysize(splitsize)
+
+    for i in range(1, num_workers+1):
+        w = WorkerNode(i+2000)
+        filenam = os.path.splitext(filename)[0]
+        tempfilename = str(filenam) + '_' + str(i) + '.txt'
+        w.Write(outputpath, tempfilename)
+
+    shutil.rmtree(outputpath)
+
+
+def Read(filename):
+        pass
+
+
+Write('input.txt', 3)
